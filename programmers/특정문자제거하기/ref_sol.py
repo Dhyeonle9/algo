fd50e8e2249c7300959176b87ff2b6ec5bb30e95
Dohyeon <dhyeon.le9@gmail.com>
2023-10-10 15:48:36 +0900

    algo

---
+++ programmers/특정문자제거하기/ref_sol.py
@@ -0,0 +1,15 @@
+# def solution(my_string, letter):
+#     answer = my_string.replace(letter,'')
+#     return answer
+
+def solution(my_string, letter):
+    answer = ''
+
+    for string in my_string:
+        if string != letter:
+            answer += string 
+    return answer
+
+
+print(solution("abcdef", "f"))
+print(solution("BCBdbe", "B"))
