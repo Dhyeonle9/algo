eab240a9c454fa67dd17664fad80f6fd4fdbdcfe
Dohyeon <dhyeon.le9@gmail.com>
2023-10-17 09:10:54 +0900

    algo

--- swea/0000_input/sol.py
+++ swea/0000_input/sol.py
@@ -26,3 +26,28 @@ numbers = list(map(int, input().split()))
 for number in numbers:
     if number % 2 == 1:
         print(f'{number}는 홀수입니다.')
+
+# 2차원 리스트 입력
+N =int(input())
+
+matrix = []
+result = 0
+for i in range(N):
+    numbers = list(map(int, input().split()))
+    matrix.append(numbers)
+
+# for row in matrix:
+#     for item in row:
+#         if item % 2:
+#             result += item
+# print(result)
+
+# 행우선반복 (index 접근) 가로
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+        print(i, j, matrix[i][j])
+
+# 열우선반복 (index 접근) 세로
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+        print(i, j, matrix[i][j])
--- swea/4835_구간합/sol.py
+++ swea/4835_구간합/sol.py
@@ -2,16 +2,23 @@ import sys
 sys.stdin = open('input.txt')
 
 T = int(input())
-for tc in range(1, T+1):
-    N, M = map(int, input().split(' '))
-    numbers = list(map(int, input().split(' ')))
-    max = numbers[0]
-    min = numbers[0]
 
-    for n in range (N-M, 0, -1):
-        for i in range(n):
-            if numbers[i]+numbers[i+1]+numbers[i+2] > max:
-                max = numbers[i]+numbers[i+1]+numbers[i+2]
-            if numbers[i]+numbers[i+1]+numbers[i+2] < min:
-                min = numbers[i]+numbers[i+1]+numbers[i+2]
-    print(min, max)
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    numbers = list(map(int, input().split()))
+    max = 0
+    min = 1000000
+    result = 0
+
+    
+    for n in range (0, N-M+1):
+        sum = 0
+        for i in range(M):
+            sum += numbers[n+i]
+        if sum > max:
+            max = sum
+        if sum < min:
+            min = sum
+    result = max - min
+
+    print(f'#{tc} {result}')
