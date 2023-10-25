0244a1f8bb9c1cd27a09beb16b0bf24c518edf5f
Dhyeonle9 <145949802+Dhyeonle9@users.noreply.github.com>
2023-10-25 16:10:33 +0900

    [level unrated] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,14 @@
+def solution(rsp):
+    answer = ''
+    for i in rsp:
+        if i == '2':
+            answer += '0'
+        elif i == '0':
+            answer += '5'
+        else:
+            answer += '2'
+
+    return answer
+
+print(solution("2"))
+print(solution("205"))
