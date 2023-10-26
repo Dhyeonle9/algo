0c58ecdf6fabdfa7ab5a0d3925919a18c68bebef
Dhyeonle9 <145949802+Dhyeonle9@users.noreply.github.com>
2023-10-26 23:43:44 +0900

    [level unrated] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/unrated/120884. 치킨 쿠폰/README.md
+++ 프로그래머스/unrated/120884. 치킨 쿠폰/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 치킨 쿠폰 - 120884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120884) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120884#) 
 
 ### 성능 요약
 
-메모리: 9.98 MB, 시간: 0.00 ms
+메모리: 9.95 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 23:41:10
+2023년 10월 4일 23:43:42
 
 ### 문제 설명
 
--- 프로그래머스/unrated/120884. 치킨 쿠폰/치킨 쿠폰.py
+++ 프로그래머스/unrated/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -1,12 +1,20 @@
+# def solution(chicken):
+#     answer = 0
+#     coupon = 0
+#     while chicken:
+#         chicken, c = divmod(chicken, 10)
+#         answer += chicken
+#         coupon += c
+#         if coupon >= 10:
+#             answer += coupon // 10
+#             coupon = coupon % 10 + coupon // 10
+#     return answer
+
+
 def solution(chicken):
     answer = 0
-    coupon = 0
-    while chicken:
-        chicken, c = divmod(chicken, 10)
+    while chicken >= 10:
+        chicken, coupon = divmod(chicken, 10)
         answer += chicken
-        coupon += c
-        if coupon >= 10:
-            answer += coupon // 10
-            coupon = coupon % 10 + coupon // 10
-  
+        chicken += coupon
     return answer
