49953352832f5fb935aa3886a2dc628cecf33947
Dhyeonle9 <145949802+Dhyeonle9@users.noreply.github.com>
2023-10-26 23:41:11 +0900

    [level unrated] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 9.98 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,12 @@
+def solution(chicken):
+    answer = 0
+    coupon = 0
+    while chicken:
+        chicken, c = divmod(chicken, 10)
+        answer += chicken
+        coupon += c
+        if coupon >= 10:
+            answer += coupon // 10
+            coupon = coupon % 10 + coupon // 10
+  
+    return answer
