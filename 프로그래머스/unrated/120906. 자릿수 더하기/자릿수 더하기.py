9329b8b4d84b92741abc0d9e4bef483673b4472a
Dhyeonle9 <145949802+Dhyeonle9@users.noreply.github.com>
2023-10-26 17:21:22 +0900

    [level unrated] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120906. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/unrated/120906. 자릿수 더하기/자릿수 더하기.py
@@ -1,6 +1,9 @@
-def solution(n):
-    answer = 0
+# def solution(n):
+#     answer = 0
+#     for num in str(n):
+#         answer += int(num)
+#     return answer
 
-    for num in str(n):
-        answer += int(num)
-    return answer
+# 한줄코드
+def solution(n):
+    return sum(list(map(int,str(n))))
