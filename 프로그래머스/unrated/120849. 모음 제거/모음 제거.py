324b793251396f5a4dfc32c9ebd41109e130a51a
Dhyeonle9 <145949802+Dhyeonle9@users.noreply.github.com>
2023-10-26 17:10:46 +0900

    [level unrated] Title: 모음 제거, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.py
@@ -1,6 +1,3 @@
 def solution(my_string):
     vowels = 'aeiou'
-    for vowel in vowels:
-        my_string = my_string.replace(vowel, '')
-    answer = my_string     
-    return answer
+    return ''.join([i for i in my_string if not i in vowels])
