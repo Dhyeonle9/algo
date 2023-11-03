c75a99d6f392a141a296ffff0be72bca80c17f8c
Dohyeon Lee <145949802+Dhyeonle9@users.noreply.github.com>
2023-11-03 20:23:16 +0900

    [level unrated] Title: 외계어 사전, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/unrated/120869. 외계어 사전/외계어 사전.py
@@ -1,19 +1,32 @@
+# def solution(spell, dic):
+#     answer = 0
+#     for i in dic:
+#         if len(spell) == len(i):
+#             for j in spell:
+#                 if j in i:
+#                     continue
+#                 else:
+#                     answer = 2
+#                     break
+#             else:
+#                 answer = 1
+#                 break
+#         else:
+#             answer = 2
+#             continue               
+#     return answer
+
 def solution(spell, dic):
-    answer = 0
-    check = 0
     for i in dic:
-        if len(spell) == len(i):
-            for j in spell:
-                if j in i:
-                    continue
-                else:
-                    answer = 2
-                    break
-            else:
-                answer = 1
-                break
-        else:
-            answer = 2
+        count_num = 0
+        for j in spell:
+            if i.count(j) == 1:
+                count_num += 1
+                print(count_num)
+        if count_num != len(spell):
             continue
-                
-    return answer
+        elif count_num == len(spell):
+            return 1
+        
+
+    return 2
