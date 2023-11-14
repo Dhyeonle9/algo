06ecb059dbcf13421e965b0b9f8156628558930e
Dohyeon Lee <145949802+Dhyeonle9@users.noreply.github.com>
2023-11-15 00:24:48 +0900

    [level 1] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,9 +1,6 @@
 def solution(a, b, n):
     answer = 0
-    temp = 0
     while n >= a:
         answer += (n//a) * b
-        temp = n % a
-        n = (n//a) * b + temp
-
+        n = (n//a) * b + n % a
     return answer 
