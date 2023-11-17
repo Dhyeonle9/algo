2cc83b42d897715fe08d423d469707ae30491175
Dohyeon Lee <145949802+Dhyeonle9@users.noreply.github.com>
2023-11-17 21:09:39 +0900

    [level unrated] Title: 안전지대, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120866. 안전지대/안전지대.py
+++ 프로그래머스/unrated/120866. 안전지대/안전지대.py
@@ -1,9 +1,37 @@
+# def solution(board):
+#     answer = 0
+#     n = len(board)
+#     danger = [(1,1), (1,0), (1,-1), (0,1), (0,-1), (-1,1), (-1,0), (-1,-1)]
+#     for i in range(n):
+#         for j in range(n):
+#             if board[i][j] == 1:
+#                 for x, y in danger:
+#                     if (0<=i+x<n and 0<=j+y<n) and board[i+x][j+y] == 0:
+#                         board[i+x][j+y] = 2
+#     for i in board:
+#         answer += i.count(0)
+#     return answer
 def solution(board):
-    answer = 0
-    for i in range(len(board)):
-        for j in range(len(board[0])//2):
-            if board[i][j] == 1:
-                if -len(board[0])//2 + 1 <= i <= len(board[0])//2 - 1 and -len(board[0])//2 + 1 <= j <= len(board[0])//2 - 1:
-                       board[i-1:i+2][j-1:j+2] = 1   
+    n = len(board)
+    mat = [[0]*(n+2) for _ in range(n+2)]
 
-    return board.count(0)
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == 1:
+                mat[i][j] = 1
+                mat[i][j+1] = 1
+                mat[i][j+2] = 1
+                mat[i+1][j] = 1
+                mat[i+1][j+1] = 1
+                mat[i+1][j+2] = 1
+                mat[i+2][j] = 1
+                mat[i+2][j+1] = 1
+                mat[i+2][j+2] = 1
+
+    count = 0
+    for i in range(n+2):
+        for j in range(n+2):
+            if 0<i<n+1 and 0<j<n+1 and mat[i][j] == 0:
+                count += 1
+    
+    return count
