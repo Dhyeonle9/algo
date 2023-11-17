ac09ee70ccbc7956a86e4024a2f8f5989a5270a7
Dohyeon Lee <145949802+Dhyeonle9@users.noreply.github.com>
2023-11-17 21:41:30 +0900

    [level unrated] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/unrated/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,5 +1,14 @@
+# def solution(num, total):
+#     if num % 2:
+#         return list(range(total//num-num//2, total//num-num//2+num))
+#     else:
+#         return list(range(total//num-num//2+1, total//num-num//2+num+1))
+    
 def solution(num, total):
-    if num % 2:
-        return list(range(total//num-num//2, total//num-num//2+num))
-    else:
-        return list(range(total//num-num//2+1, total//num-num//2+num+1))
+    answer = []
+    num_list = list(range(-50, 500))
+    for i in range(len(num_list)):
+        if sum(num_list[i:i+num]) == total:
+            answer = num_list[i:i+num]
+            break
+    return answer
