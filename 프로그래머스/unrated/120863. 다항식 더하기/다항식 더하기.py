6028f97989f65ba9cb093da240cf0a7eddf7588f
Dohyeon Lee <145949802+Dhyeonle9@users.noreply.github.com>
2023-11-17 09:03:56 +0900

    [level unrated] Title: 다항식 더하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120863. 다항식 더하기/다항식 더하기.py
+++ 프로그래머스/unrated/120863. 다항식 더하기/다항식 더하기.py
@@ -1,31 +1,63 @@
+# def solution(polynomial):
+#     answer = polynomial.split(' + ')
+#     fst=0
+#     const = 0
+#     for pol in answer:
+#         if pol[-1] == 'x':
+#             if len(pol) > 1:
+#                 pol = pol.strip('x')
+#                 fst += int(pol)
+#             else:
+#                 fst += 1
+#         else:
+#             pol = pol.strip('')
+#             const += int(pol)
+#     if fst == 0:
+#         if const == 0:
+#             return 0
+#         else:
+#             return str(const)
+
+#     elif fst == 1:
+#         if const == 0:
+#             return 'x'
+#         else:
+#             return 'x + ' + str(const)
+
+#     else:
+#         if const == 0:
+#             return str(fst) + 'x'
+#         else:
+#             return str(fst) + 'x + ' + str(const)
+
 def solution(polynomial):
-    answer = polynomial.split(' + ')
-    fst=0
-    const = 0
-    for pol in answer:
-        if pol[-1] == 'x':
-            if len(pol) > 1:
-                pol = pol.strip('x')
-                fst += int(pol)
+    num = 0     # 7
+    strr = 0    # ' x'
+    polynomial = polynomial.split('+') # ['3x ', ' 7 ', ' x'], ['x ', ' x ', ' x']
+
+    for i in polynomial:
+        term = i.strip()
+        if term.isdigit():
+            num += int(term)
+        else:
+            if term == 'x':
+                strr += 1
             else:
-                fst += 1
+                strr += int(term[:-1])
+    if strr == 0:
+        return str(num)
+    elif strr == 1:
+        if num != 0:
+            return 'x + ' + str(num)
         else:
-            pol = pol.strip('')
-            const += int(pol)
-    if fst == 0:
-        if const == 0:
-            return 0
-        else:
-            return str(const)
-
-    elif fst == 1:
-        if const == 0:
             return 'x'
-        else:
-            return 'x + ' + str(const)
-
     else:
-        if const == 0:
-            return str(fst) + 'x'
+        if num != 0:
+            return f'{strr}x + {num}'
         else:
-            return str(fst) + 'x + ' + str(const)
+            return f'{strr}x'
+    
+
+
+print(solution("3x + 7 + x"))
+print(solution("x + x + x"))
