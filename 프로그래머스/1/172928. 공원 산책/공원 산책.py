3349e241d09ffd8ae316bb40f84fc292932ed1ef
Dohyeon Lee <145949802+Dhyeonle9@users.noreply.github.com>
2023-12-01 11:21:49 +0900

    [level 1] Title: 공원 산책, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -0,0 +1,25 @@
+def solution(park, routes):
+    loc = [0, 0]
+    move = {
+        'E': [0, 1],
+        'W': [0, -1],
+        'S': [1, 0],
+        'N': [-1, 0],
+    }
+    for i, road in enumerate(park):
+        if 'S' in road:
+            loc = [i, road.find('S')]
+            break
+            
+    for route in routes:
+        d, s = route.split()
+        cur_loc = [loc[0], loc[1]]
+        for i in range(int(s)):
+            new_loc = [loc[0] + move[d][0], loc[1] + move[d][1]]
+            if 0<=new_loc[0]<=len(park)-1 and 0<=new_loc[1]<=len(park[0])-1 and park[new_loc[0]][new_loc[1]] != 'X':
+                    loc = [new_loc[0], new_loc[1]]
+            else:
+                loc = [cur_loc[0], cur_loc[1]]
+                break
+                    
+    return loc
