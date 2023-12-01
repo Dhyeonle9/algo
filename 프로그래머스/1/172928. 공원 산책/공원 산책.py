abd43d948b35086d54910ee2743e7f0074ba38ba
Dohyeon Lee <145949802+Dhyeonle9@users.noreply.github.com>
2023-12-01 21:08:59 +0900

    [level 1] Title: 공원 산책, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -13,13 +13,13 @@ def solution(park, routes):
             
     for route in routes:
         d, s = route.split()
-        cur_loc = [loc[0], loc[1]]
+        cur_loc = loc
         for i in range(int(s)):
             new_loc = [loc[0] + move[d][0], loc[1] + move[d][1]]
             if 0<=new_loc[0]<=len(park)-1 and 0<=new_loc[1]<=len(park[0])-1 and park[new_loc[0]][new_loc[1]] != 'X':
                     loc = [new_loc[0], new_loc[1]]
             else:
-                loc = [cur_loc[0], cur_loc[1]]
+                loc = cur_loc
                 break
                     
     return loc
