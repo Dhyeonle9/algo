db9a914c5c8f64414c260bb1b99e9602c22ef6f2
Dohyeon Lee <145949802+Dhyeonle9@users.noreply.github.com>
2023-12-08 20:12:54 +0900

    [level 2] Title: 올바른 괄호, Time: 8.77 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,18 +1,13 @@
 def solution(s):
-    answer = True
     stack = []
     for char in s:
         if char == '(':
-            stack.append(char)
-            
+            stack.append(char)           
         elif char == ')':
             if stack and stack[-1] == '(':
                 stack.pop()
-                answer = True
             else:
                 return False
-        
     if stack:
         return False
-
-    return answer
+    return True
