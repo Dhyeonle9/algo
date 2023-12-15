b09461ea53ec8e835e45a529aca4fb570b75e131
Dohyeon Lee <145949802+Dhyeonle9@users.noreply.github.com>
2023-12-15 21:45:37 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -2,6 +2,8 @@ def solution(citations):
     answer = 0
     citations.sort(reverse=True)
     for i in range(len(citations)):
-        if i >= citations[i]:
-            return i                    
+        if i+1 > citations[i]:
+            print(i+1, citations[i])
+            return i
+        print(i+1, citations[i])
     return len(citations)
