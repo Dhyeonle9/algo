07e05cd9e6c03f80bb3ff20eade7e0cd93ebb9b2
Dohyeon Lee <145949802+Dhyeonle9@users.noreply.github.com>
2024-01-19 19:28:21 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,38 @@
+def solution(genres, plays):
+    answer = []
+    gen_dic = {}
+    music = {}
+    count = 0
+# 장르별 재생횟수 dic gen_dic
+    for g, p in zip(genres, plays):       
+        if g in gen_dic:
+            gen_dic[g] += p
+        else:
+            gen_dic[g] = p
+            
+# 장르에따른 노래 고유번호, 재생횟수 dic music
+    for i in range(len(plays)):
+        if genres[i] in music:
+            music[genres[i]].append((i, plays[i]))
+        else:
+            music[genres[i]] = [(i, plays[i])]
+            
+    gen_dic = sorted(gen_dic.items(), key=lambda x:x[1], reverse=True)            
+    
+    for k, v in gen_dic:
+        
+        mu = sorted(music[k], key=lambda x:x[1], reverse=True)
+        
+        print(k)
+        count = 0
+        for i in mu:
+            print(count)
+            if count >= 2:
+                
+                break
+            answer.append(i[0])
+            count += 1
+
+            
+            
+    return answer
