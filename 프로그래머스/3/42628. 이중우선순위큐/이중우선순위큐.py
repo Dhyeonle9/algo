2ba22c17752781ab1dd86dc2ff7edb008247ccb3
Dohyeon Lee <145949802+Dhyeonle9@users.noreply.github.com>
2024-01-19 19:13:00 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,3 +1,26 @@
+# import heapq
+
+# def solution(operations):
+#     answer = []
+#     heap = []
+#     for op in operations:
+#         o, num = op.split()
+#         num = int(num)
+
+#         if o == 'I':
+#             heapq.heappush(heap, num)
+#         elif o == 'D':
+#             if num == 1 and len(heap)>0:
+#                 heap.remove(heap[-1])
+#             elif num == -1 and len(heap)>0:
+#                 heapq.heappop(heap)
+
+#     if len(heap)!= 0:
+#         answer = [heap[-1], heapq.heappop(heap)]
+#     else: 
+#         answer = [0, 0]
+#     return answer
+
 import heapq
 
 def solution(operations):
