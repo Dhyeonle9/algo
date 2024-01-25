c7b73b74c9fd6f20cb1a9deeed1bf79d64dc26e8
Dohyeon Lee <145949802+Dhyeonle9@users.noreply.github.com>
2024-01-25 09:23:30 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int solution(int slice, int n) {
     int answer = 0;
-    if (n%slice){
+    if (n%slice)
         return n/slice+1;
-    }
+    
     else
         return n/slice;
     
