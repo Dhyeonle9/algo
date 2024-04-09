cfe9eacc3d2ee619663363f96f415383b06d9c35
Dohyeon Lee <145949802+Dhyeonle9@users.noreply.github.com>
2024-04-10 08:17:10 +0900

    [Bronze II] Title: 문자열 분석, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10820. 문자열 분석/문자열 분석.py
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.py
@@ -1,18 +1,20 @@
 import sys
 
 while True :
-    try :
-        lst = list(input())
-        lower , upper, num, blank = 0,0,0,0
-        for i in range(len(lst)) :
-            if lst[i] == " " :
+    line = sys.stdin.readline().rstrip('\n')
+
+    if not line:
+        break
+    
+    
+    lower , upper, num, blank = 0,0,0,0
+    for i in range(len(line)) :
+            if line[i] == " " :
                 blank += 1
-            elif 65 <= ord(lst[i]) <= 90 :
+            elif line[i].isupper() :
                 upper += 1
-            elif 97 <= ord(lst[i]) <= 122 :
+            elif line[i].islower() :
                 lower += 1
             else :
                 num += 1
-        print(lower,upper,num,blank)
-    except EOFError :
-        break
+    print(lower,upper,num,blank)
