795a0809907f784d8a700c9bb40f6d39de4d909d
Dohyeon Lee <145949802+Dhyeonle9@users.noreply.github.com>
2024-04-11 08:36:06 +0900

    [Bronze V] Title: 나머지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.py
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -1,7 +1,6 @@
 import sys
-
-line = input().split()
-A, B, C = int(line[0]), int(line[1]), int(line[2])
+line = list(map(int, input().split()))
+A, B, C = line[0], line[1], line[2]
 print((A+B)%C)
 print(((A%C) + (B%C))%C)
 print((A*B)%C)
