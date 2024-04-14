4d159ebbebe4d33e3e8280bb648500b45c649033
Dhyeonle9 <dhyeon.le9@gmail.com>
2024-04-15 08:19:13 +0900

    알고리즘문제풀이

---
+++ 백준/알고리즘기초/1934최소공배수/sol.py
@@ -0,0 +1,20 @@
+import sys
+
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+
+def lcm(x, y):
+    result = (x * y) // gcd(x, y)
+    return result
+
+sys.stdin = open('input.txt')
+
+n = int(input())
+
+for i in range(n):
+    x, y = map(int, input().split())
+
+
+    print(lcm(x, y))
