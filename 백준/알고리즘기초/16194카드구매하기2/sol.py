77b0c8c3fedb16580848d17017013c6d4883f82a
Dhyeonle9 <dhyeon.le9@gmail.com>
2024-05-06 08:42:25 +0900

    알고리즘풀이

--- 백준/알고리즘기초/16194카드구매하기2/sol.py
+++ 백준/알고리즘기초/16194카드구매하기2/sol.py
@@ -10,7 +10,7 @@ dp = [0 for _ in range(N+1)]
 for i in range(1, N+1):
     for j in range(1, i+1):
         if dp[i] == 0 :
-            dp[i] = dp[i-k]+p[k]
+            dp[i] = dp[i-j]+p[j]
         else :
-            dp[i] = min(dp[i], dp[i-k]+p[k])
+            dp[i] = min(dp[i], dp[i-j]+p[j])
 print(dp[N])
