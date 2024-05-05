375092530d59b3f49f9c5f64edf791301ea92590
Dhyeonle9 <dhyeon.le9@gmail.com>
2024-05-06 08:37:35 +0900

    알고리즘풀이

---
+++ 백준/알고리즘기초/16194카드구매하기2/sol.py
@@ -0,0 +1,16 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+N = int(input())
+
+p = [0] + list(map(int,input().split()))
+dp = [0 for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, i+1):
+        if dp[i] == 0 :
+            dp[i] = dp[i-k]+p[k]
+        else :
+            dp[i] = min(dp[i], dp[i-k]+p[k])
+print(dp[N])
